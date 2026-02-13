dba4cf61f36fa0ebffaf3c6677937f1d15673b9f
Itswali <luckywali0986@gmail.com>
2026-02-13 18:13:12 +0500

    now going to initialize the zustand store to handle functionality of creating and displaying the items

--- src/store/useCounterStore.ts
+++ src/store/useCounterStore.ts
@@ -2,11 +2,9 @@
 import { create } from 'zustand'
 
 interface CounterState {
-  addItem: () => void
-  display: () => void
+  createItem: () => void
+  viewItem: () => void
 }
 
 export const useCounterStore = create<CounterState>((set) => ({
-  // addItem:,
-  // reset: () => set({ count: 0 }),
 }))
