c5bd48841654dd5318e3b694ff97783a4f98c7a3
Itswali <luckywali0986@gmail.com>
2026-02-16 18:44:46 +0500

    fix 1 delete all same item by seperating item and cart

--- src/store/useCounterStore.ts
+++ src/store/useCounterStore.ts
@@ -11,12 +11,20 @@ interface Item {
   price: number;
   details: string;
 }
+interface Citem {
+  id: string;
+  name: string;
+  type: string;
+  image: string;
+  price: number;
+  details: string;
+}
 interface ItemStore {
   items: Item[];
-  cart: Item[];
+  cart: Citem[];
   addItem: (newItem: Omit<Item, 'id'>) => void;
   deleteItem: (itemId: 'id') => void;
-  addToCart: (item: Item) => void;
+  addToCart: (newCartItem: Omit<Citem, 'id'>) => void;
   removeFromCart: (itemId: string) => void;
 }
 export const useCartStore = create<ItemStore>()(
@@ -35,7 +43,11 @@ export const useCartStore = create<ItemStore>()(
         cart: state.cart.filter((item) => item.id !== itemId)
       })),
       addToCart: (item) => set((state) => ({
-        cart: [...state.cart, item]
+        cart: [
+          ...state.cart,
+          {...item, id:crypto.randomUUID() },
+        ],
+        // cart: [...state.cart, item,]
       })),
       removeFromCart: (itemId) => set((state) => ({
         cart: state.cart.filter((item) => item.id !== itemId)
